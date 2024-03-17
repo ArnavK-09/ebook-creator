02032f4bea1f85fb7c4077d0a16b3ede9f59a649
Arnav K <arnavkaushal09@gmail.com>
2024-03-17 14:36:05 +0000

    Fix

--- src/main.ts
+++ src/main.ts
@@ -9,28 +9,56 @@ import GeminiAI from "./providers/gemini";
  */
 const form = document.getElementsByTagName("form")[0] as HTMLFormElement;
 const input = document.getElementById("book_motive") as HTMLInputElement;
-const btn = document.getElementById("submitBTN") as HTMLButtonElement;
+const submit_btn = document.getElementById("submitBTN") as HTMLButtonElement;
+const download_link = document.getElementById(
+  "download_link",
+) as HTMLAnchorElement;
+const download = document.getElementById("download") as HTMLDivElement;
 
 /**
  * Function to creeate e - book
  */
 const createEBOOK = async (event: SubmitEvent | MouseEvent) => {
-	event.preventDefault();
-	if (input.value.trim().length === 0) return;
-	toggleLoading();
-	const ai = new GeminiAI(input.value);
-	await ai
-		.startup()
-		.then(async () => await ai.renderBook())
-		.catch((e: Error) => {
-			ai.logUI(e.message, "error");
-			ai.logger.error(e);
-		})
-		.finally(toggleLoading);
+  event.preventDefault();
+  if (input.value.trim().length === 0) return;
+  toggleLoading();
+  const ai = new GeminiAI(input.value);
+  await ai
+    .startup()
+    .then(async () => await ai.renderBook())
+    .catch((e: Error) => {
+      ai.logUI(e.message, "error");
+      ai.logger.error(e);
+    })
+    .finally(toggleLoading);
 };
 
 /**
  * Appending Event Listeners
  */
 form.addEventListener("submit", createEBOOK);
-btn.addEventListener("click", createEBOOK);
+submit_btn.addEventListener("click", createEBOOK);
+
+/**
+ * Appends download link of text from sring
+ * @param text
+ */
+export const downloadLinkFromText = (text: string) => {
+  // Create a Blob object from the text content
+  var blob = new Blob([text], { type: "text/plain" });
+
+  // Create a temporary URL for the Blob object
+  var url = URL.createObjectURL(blob);
+
+  // Create a link element
+  download_link.href = url;
+  download.style.display = "grid";
+  download_link.download = "ebook.html";
+
+  // new window
+  const newWindow = window.open();
+  newWindow?.document.write(text);
+
+  // Simulate a click on the link to trigger the download
+  // link.click();
+};
